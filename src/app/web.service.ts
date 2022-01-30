2ca1f5c010df023b04bf8a22f259592c79ec365d
Anirban2231 <anirbanmukherjee15@gmail.com>
2022-01-30 12:11:53 +0530

    next add require httpheader,httpclent and a rooturl set up in web service.Adding get and post method for required api calls

--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpHeaders, HttpClient, HttpHandler } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WebService {
+  readonly ROOT_URL: any
+  constructor(private http: HttpClient) { 
+    this.ROOT_URL = ''
+  }
+  get(uri: string, headers: HttpHeaders): Observable<any> {
+    return  this.http.get(`${this.ROOT_URL}/${uri}`, {headers})
+   }
 
-  constructor() { }
+   post(uri: string, payload: Object, headers: HttpHeaders) {
+    //
+      return this.http.post(`${this.ROOT_URL}${uri}`, payload, { headers})
+  }
 }

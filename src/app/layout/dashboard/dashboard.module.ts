3e0c8e539d08d9a63deb37e57dcf4bd7d1679dce
Anirban2231 <anirbanmukherjee15@gmail.com>
2022-01-30 14:19:00 +0530

    add all the child components in dashbaord component in layout durectory

--- src/app/layout/dashboard/dashboard.module.ts
+++ src/app/layout/dashboard/dashboard.module.ts
@@ -6,11 +6,21 @@ import { DashboardComponent } from './dashboard.component';
 import { SharedModule } from 'src/app/shared/shared/shared.module';
 import { RouterModule } from '@angular/router';
 import { NgChartsModule } from 'ng2-charts';
+import { DashboardLayoutComponent } from 'src/app/pages/dashboard-layout/dashboard-layout.component';
+import { AddExpensesComponent } from 'src/app/pages/add-expenses/add-expenses.component';
+import { AddIncomeComponent } from 'src/app/pages/add-income/add-income.component';
+import { ReportComponent } from 'src/app/pages/report/report.component';
+import { TransactionComponent } from 'src/app/pages/transaction/transaction.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    DashboardLayoutComponent,
+    AddExpensesComponent,
+    AddIncomeComponent,
+    ReportComponent,
+    TransactionComponent
   ],
   imports: [
     CommonModule,

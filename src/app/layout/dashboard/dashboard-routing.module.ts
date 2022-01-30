9cfaa810fec501acd8b2cb55311f629ea88df462
Anirban2231 <anirbanmukherjee15@gmail.com>
2022-01-30 13:19:09 +0530

    set a routing for dashboard-routing module and add set routing for components in pages directory

--- src/app/layout/dashboard/dashboard-routing.module.ts
+++ src/app/layout/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddExpensesComponent } from 'src/app/pages/add-expenses/add-expenses.component';
+import { AddIncomeComponent } from 'src/app/pages/add-income/add-income.component';
+import { DashboardLayoutComponent } from 'src/app/pages/dashboard-layout/dashboard-layout.component';
+import { ReportComponent } from 'src/app/pages/report/report.component';
+import { TransactionComponent } from 'src/app/pages/transaction/transaction.component';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent 
+  { 
+    path: '', 
+    component: DashboardComponent,
+    children:[
+      {
+        path: '',
+        component: DashboardLayoutComponent
+      },
+      {
+        path: '/add-income',
+        component: AddIncomeComponent
+      },
+      {
+        path: '/add-expenses',
+        component: AddExpensesComponent
+      },
+      {
+        path: '/report',
+        component: ReportComponent
+      },
+      {
+        path: '/transaction',
+        component: TransactionComponent
+      }
+    ] 
 
 }
 

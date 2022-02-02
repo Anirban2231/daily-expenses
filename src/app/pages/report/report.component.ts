f5842e3559204749da289393637692fd51fe09ec
Anirban2231 <anirbanmukherjee15@gmail.com>
2022-02-02 10:28:33 +0530

    implement report section where we can see income,expenses,balance yearly and adding filter for filtering the data year and month wise

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, QueryList, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
+import { ReportService } from './report.service';
 
 @Component({
   selector: 'app-report',
@@ -6,10 +9,114 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
+  showChart: boolean = false
+  income: any
+  expense: any
+  balance: any
+  dataPie: any = []
+  years: any =[]
+  months: any= []
+  monthsByName : any = ['Jan','Feb','Mar','Apr','May','June','Jul','Aug','Sep','Oct','Dec']
+  @ViewChild(BaseChartDirective) charts?: BaseChartDirective;
+   /***********************************Pie Chart*********************************************/
+   public pieChartOptions: ChartConfiguration['options'] = {
+    responsive: true,
+    plugins: {
+      legend: {
+        display: true,
+        position: 'top',
+      },
+    }
+  };
+  public pieChartData: ChartData<'pie', number[], string | string[]> = {
+    labels: [ 'Income','Expenses','Balance' ],
+    datasets: [ {
+      data: []
+    } ]
+  };
+  public pieChartType: ChartType = 'pie';
+  /**************************************Pie chart*******************************************/
+  constructor(private service : ReportService) { 
 
-  constructor() { }
+      this.service.getallYear().subscribe(
+        (data) =>{
+          console.log(data)
+          this.years = data['years']
+        }
+      )
+      this.service.getAllMonth().subscribe(
+        (data) =>{
+          console.log(data)
+          this.months = data['months']
+        }
+      )
+   }
 
   ngOnInit(): void {
+
+    var date = new Date();
+    var year = date.getUTCFullYear();
+
+    this.service.getCurrentyearAnalysis(year).subscribe(
+      (data) =>{
+
+          console.log(data)
+          this.dataPie=data
+          this.pieChartData.datasets[0].data[0] = this.dataPie['income']
+          this.pieChartData.datasets[0].data[1] = this.dataPie['expence']
+          this.pieChartData.datasets[0].data[2] = this.dataPie['balance']
+
+          this.showChart= true
+          this.income = this.dataPie['income']
+          this.expense = this.dataPie['expence']
+          this.balance =  this.dataPie['balance']
+
+      }
+    )
+  }
+  
+  filterByYear(event:any){
+    
+    this.service.getCurrentyearAnalysis(event.target.value).subscribe(
+      (data) =>{
+
+          console.log(data)
+          this.dataPie=data
+          this.pieChartData.datasets[0].data[0] = this.dataPie['income']
+          this.pieChartData.datasets[0].data[1] = this.dataPie['expence']
+          this.pieChartData.datasets[0].data[2] = this.dataPie['balance']
+
+          this.showChart= true
+          this.income = this.dataPie['income']
+          this.expense = this.dataPie['expence']
+          this.balance =  this.dataPie['balance']
+
+          this.charts?.update()
+      }
+    )
+
+    
   }
 
+  filterByMonth(event:any,year:any){
+
+      this.service.getCurrentmonthAnalysis(event.target.value,year).subscribe(
+        (data)=>{
+          this.dataPie=data
+          this.pieChartData.datasets[0].data[0] = this.dataPie['income']
+          this.pieChartData.datasets[0].data[1] = this.dataPie['expence']
+          this.pieChartData.datasets[0].data[2] = this.dataPie['balance']
+
+          this.showChart= true
+          this.income = this.dataPie['income']
+          this.expense = this.dataPie['expence']
+          this.balance =  this.dataPie['balance']
+
+          this.charts?.update()
+        }
+      )
+  }
+
+  
+ 
 }
--- src/app/pages/report/report.service.ts
+++ src/app/pages/report/report.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { WebService } from 'src/app/web.service';
+import { HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReportService {
 
-  constructor() { }
+  constructor(private webService: WebService) { }
+  getCurrentyearAnalysis(year:any){
+    let headers = new  HttpHeaders ({'Content-Type': 'application/json'})
+    return this.webService.get(`get_date_by_year/${year}`, headers)
+  }
+
+  getallYear(){
+    let headers = new  HttpHeaders ({'Content-Type': 'application/json'})
+    return this.webService.get(`get_all_year`, headers)
+  }
+  getAllMonth(){
+    let headers = new  HttpHeaders ({'Content-Type': 'application/json'})
+    return this.webService.get(`get_all_month`, headers)
+  }
+
+  getCurrentmonthAnalysis(month:any,year:any){
+    let headers = new  HttpHeaders ({'Content-Type': 'application/json'})
+    return this.webService.get(`get_date_by_months/${month}/${year}`, headers)
+  }
 }
